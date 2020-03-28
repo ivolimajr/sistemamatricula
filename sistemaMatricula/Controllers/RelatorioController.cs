9e5b1d90a11bac79f2843f14beca680e6d6e36dd
IvoLimaJr <ivo.sl.jr@gmail.com>
2020-03-28 16:39:29 -0300

    Inicio de criação dos relatórios

---
+++ sistemaMatricula/Controllers/RelatorioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sistemaMatricula.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        // GET: Relatorio
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Relatorio
+        public ActionResult RelatorioTurma()
+        {
+
+            return View();
+        }
+    }
+}
